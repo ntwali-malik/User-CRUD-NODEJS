a76d7a5cdcaa44a5ba0c6c7bc29fe2d3e712cd0d
Maliki <aboulmalikntwari@gmail.com>
2025-03-11 17:03:22 +0200

    New routes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -15,10 +15,41 @@ router.post("/", async(req, res) =>{
 })
 
 // Displaying All users
-
 router.get("/", async(req,res)=> {
         const user = await User.find();
         res.json(user)
 })
+
+// Getting user by ID
+
+router.get("/:id", async(req, res) => {
+    try
+    {
+        const user = await User.findById(req.params.id)
+        if(!user) return res.status(404).json({message: "user not found" })
+        res.json(user)
+    }
+    catch(error)
+    {
+        res.status(400).json({error: error.message})
+    }
+})
+
+
+//update user
+
+router.put("/:id", async(req,res) => {
+    try
+    {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        if(!user) return res.status(404).json({message: "User not found"})
+        res.json(user)
+    }
+    catch(err)
+    {
+        res.status(400).json({error: error.message})
+    }
+})
+
 
 module.exports = router;
